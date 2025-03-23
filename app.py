c2d724a597ba9ac573a32b81c21a7744d8140e5e
Petr Korolev <peterkorolev95@gmail.com>
2025-03-23 15:50:20 -0700

    add dockerignore

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template(template_name_or_list='index.html')
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=8080)
