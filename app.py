797dc0e20a0b0262dc05c3ce53365ff6ee1e7e05
Petr Korolev <peterkorolev95@gmail.com>
2025-03-25 15:35:33 -0700

    added routing for extra pages and added logo to tab

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def main():
     return render_template(template_name_or_list='index.html')
 
+@app.route("/events")
+def events():
+    return render_template(template_name_or_list='events.html')
+
+@app.route("/how-to-join")
+def how_to_join():
+    return render_template(template_name_or_list='how-to-join.html')
+
+@app.route("/maps")
+def maps():
+    return render_template(template_name_or_list='maps.html')
+
+@app.route("/news")
+def news():
+    return render_template(template_name_or_list='news.html')
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
